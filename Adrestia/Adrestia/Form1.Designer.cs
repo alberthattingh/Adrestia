91db327ac487a1f9bdb0718b70b234f7260e5f98
Albert Hattingh <albert.h0504@gmail.com>
2019-08-20 11:15:13 +0200

    Made panel & button for maintaining students
    Edited main form (Form1) to include side panel and a button on side panel for accessing the Students control.

--- Adrestia/Adrestia/Form1.Designer.cs
+++ Adrestia/Adrestia/Form1.Designer.cs
@@ -28,20 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnStudents = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.btnStudents);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 666);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnStudents
+            // 
+            this.btnStudents.Location = new System.Drawing.Point(0, 124);
+            this.btnStudents.Name = "btnStudents";
+            this.btnStudents.Size = new System.Drawing.Size(200, 71);
+            this.btnStudents.TabIndex = 1;
+            this.btnStudents.Text = "Students";
+            this.btnStudents.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(925, 564);
+            this.ClientSize = new System.Drawing.Size(1184, 666);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnStudents;
     }
 }
 
